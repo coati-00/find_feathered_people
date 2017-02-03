e7ae41a802403aea5727b47706b6753e0aef96e5
Cassandra Dunlop <coati.00@gmail.com>
2017-02-03 15:29:37 -0500

    extract rescue information from detail page

--- find_feathered_people/find_feathered_people/spiders/adoptapetspider.py
+++ find_feathered_people/find_feathered_people/spiders/adoptapetspider.py
@@ -13,24 +13,53 @@ class AdoptAPetSpider(scrapy.Spider):
     def get_details(self, response):
         '''follow link to details page and get detailed info there'''
         details = {'species': '', 'color': '', 'age': '', 'gender': ''}
+        rescue = {'name': '', 'phone': '', 'email': '', 'website': '', 'address': ''}
+        birdstory = ""
+        largephoto = ""
         for element in response.css('div.container div.row'):
             birdinfobox = element.css("div.blue_highlight.no_margin.top_margin_xlarge ul li")
             for each in birdinfobox:
                 if each.css("b::text").extract() == [u'Breed']:
                     details['species'] = each.css("li::text").extract()
-                if ach.css("b::text").extract() == [u'Color:']:
+                if each.css("b::text").extract() == [u'Color:']:
                     details['color'] = each.css("li::text").extract()
-                if ach.css("b::text").extract() == [u'Age:']:
+                if each.css("b::text").extract() == [u'Age:']:
                     details['color'] = each.css("li::text").extract()
-                if ach.css("b::text").extract() == [u'Sex:']:
+                if each.css("b::text").extract() == [u'Sex:']:
                     details['gender'] = each.css("li::text").extract()
-                yield {
-                    'species' : details['species'],
-                    'color': details['color'],
-                    'age': details['color'],
-                    'gender': details['gender'],
-                    'all': birdinfobox.extract()
-                }
+        birdinfostory = response.css("div.info_box.row div div div.body").extract()
+        if birdinfostory is not None:
+            birdstory = birdinfostory
+        birdlargephoto = response.css("div.col-sm-7 div.large_image img::attr(src)").extract()
+        if birdlargephoto is not None:
+            largephoto = birdlargephoto
+
+        # Also get rescue org info
+        rescuesidebar = response.css("div.body.contact_sidebar.hidden-xs")
+
+        # Go over each of the listed items in the side bar and extract correct info
+        for each in rescuesidebar.css("ul li"):
+            if each.css("b::text").extract() == [u'Rescue Group:']:
+                rescue['name'] = each.css("a::text").extract()
+            if each.css("b::text").extract() == [u'Phone:']:
+                rescue['phone'] = each.css("a::text").extract()
+            if each.css("b::text").extract() == [u'E-mail:']:
+                rescue['email'] = each.css("a::text").extract()
+            if each.css("b::text").extract() == [u'Website:']:
+                rescue['website'] = each.css("a::text").extract()
+            if each.css("b::text").extract() == [u'Address:']:
+                rescue['address'] = each.css("li::text").extract()
+
+        yield {
+            'rescue' : rescue,
+            'largephoto' : largephoto,
+            'birdstory': birdstory,
+            'species' : details['species'],
+            'color': details['color'],
+            'age': details['color'],
+            'gender': details['gender'],
+        }
+
 
 
     def parse(self, response):
@@ -46,7 +75,7 @@ class AdoptAPetSpider(scrapy.Spider):
                 yield scrapy.Request(next_page, callback=self.get_details)
             yield {
                 'name' : initialelement.css('p a.name::text').extract(),
-                'detailslink' : initialelement.css('a::attr(href)').extract(),
+                'detailslink' : initialelement.css('a::attr(href)').extract_first(),
                 'birdgender' : initialelement.css(
                     "div.pet_results.rounded_corner > a::text").extract(),
                 'rescueorgtownstate' : initialelement.css(
