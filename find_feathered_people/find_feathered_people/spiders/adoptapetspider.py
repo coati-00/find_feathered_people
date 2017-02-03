b766b58f76365480d9debfd05355425638c4a1da
Cassandra Dunlop <coati.00@gmail.com>
2017-02-03 14:32:47 -0500

    fix broken extraction

--- find_feathered_people/find_feathered_people/spiders/adoptapetspider.py
+++ find_feathered_people/find_feathered_people/spiders/adoptapetspider.py
@@ -10,12 +10,40 @@ class AdoptAPetSpider(scrapy.Spider):
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
+    def get_details(self, response):
+        '''follow link to details page and get detailed info there'''
+        details = {'species': '', 'color': '', 'age': '', 'gender': ''}
+        for element in response.css('div.container div.row'):
+            birdinfobox = element.css("div.blue_highlight.no_margin.top_margin_xlarge ul li")
+            for each in birdinfobox:
+                if each.css("b::text").extract() == [u'Breed']:
+                    details['species'] = each.css("li::text").extract()
+                if ach.css("b::text").extract() == [u'Color:']:
+                    details['color'] = each.css("li::text").extract()
+                if ach.css("b::text").extract() == [u'Age:']:
+                    details['color'] = each.css("li::text").extract()
+                if ach.css("b::text").extract() == [u'Sex:']:
+                    details['gender'] = each.css("li::text").extract()
+                yield {
+                    'species' : details['species'],
+                    'color': details['color'],
+                    'age': details['color'],
+                    'gender': details['gender'],
+                    'all': birdinfobox.extract()
+                }
+
+
     def parse(self, response):
         '''for now save the pages - handle download for each of the
         requests made'''
         # the selector for bird gender seems to return gender
         # and age if there is an age
         for initialelement in response.css("div.pet_results.rounded_corner"):
+            detailslink = initialelement.css('a::attr(href)').extract_first()
+            if detailslink is not None:
+                next_page = response.urljoin(detailslink)
+                print "next_page"
+                yield scrapy.Request(next_page, callback=self.get_details)
             yield {
                 'name' : initialelement.css('p a.name::text').extract(),
                 'detailslink' : initialelement.css('a::attr(href)').extract(),
